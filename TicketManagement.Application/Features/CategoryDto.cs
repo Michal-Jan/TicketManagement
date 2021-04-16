4a1005711fd590fe5ba876b96bbbbefdbfc242cc
Michal Januszko <michaljanuszko@gmail.com>
2021-04-16 13:01:21 +0200

    Added App Features with AutoMapper and MediatR.

---
+++ TicketManagement.Application/Features/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TicketManagement.Application.Features
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ TicketManagement.Application/Features/GetEventsListQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TicketManagement.Application.Contracts.Persistence;
+using TicketManagement.Domain.Entities;
+
+namespace TicketManagement.Application.Features
+{
+    public class GetEventsListQueryHandler : IRequestHandler<GetEventsListQuery, List<EventListVm>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IAsyncRepository<Event> _eventRepository;
+
+        public GetEventsListQueryHandler (IMapper mapper, IAsyncRepository<Event> eventRepository)
+        {
+            _mapper = mapper;
+            _eventRepository = eventRepository;
+        }
+        
+        public async Task<List<EventListVm>> Handle(GetEventsListQuery request, CancellationToken cancellationToken)
+        {
+            var allEvents = (await _eventRepository.ListAllAsync()).OrderBy(x => x.Date);
+            return _mapper.Map<List<EventListVm>>(allEvents);
+        }
+    }
+}
