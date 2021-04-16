f7b10b7b2d94af19be2d147bfb5611283198d64d
Michal Januszko <michaljanuszko@gmail.com>
2021-04-16 14:07:23 +0200

    Added Event commands to Application

---
+++ TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using TicketManagement.Application.Contracts.Persistence;
+using TicketManagement.Domain.Entities;
+
+namespace TicketManagement.Application.Features.Events.Commands.CreateEvent
+{
+    public class CreateEventCommandHandler : IRequestHandler<CreateEventCommand, Guid>
+    {
+        private readonly IMapper _mapper;
+        private readonly IAsyncRepository<Event> _eventRepository;
+
+        public CreateEventCommandHandler(IMapper mapper, IAsyncRepository<Event> eventRepository)
+        {
+            _mapper = mapper;
+            _eventRepository = eventRepository;
+        }
+        
+        public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
+        {
+            var @event = _mapper.Map<Event>(request);
+            
+            @event = await _eventRepository.AddAsync(@event);
+
+            return @event.EventId;
+        }
+    }
+}
--- TicketManagement.Application/Profiles/MappingProfile.cs
+++ TicketManagement.Application/Profiles/MappingProfile.cs
@@ -2,6 +2,9 @@
 using TicketManagement.Application.Features;
 using TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
 using TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
+using TicketManagement.Application.Features.Events.Commands.CreateEvent;
+using TicketManagement.Application.Features.Events.Commands.DeleteEvent;
+using TicketManagement.Application.Features.Events.Commands.UpdateEvent;
 using TicketManagement.Domain.Entities;
 
 namespace TicketManagement.Application.Profiles
@@ -12,9 +15,13 @@ namespace TicketManagement.Application.Profiles
         {
             CreateMap<Event, EventListVm>().ReverseMap();
             CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Event, CreateEventCommand>().ReverseMap();
+            CreateMap<Event, UpdateEventCommand>().ReverseMap();
+            CreateMap<Event, DeleteEventCommand>().ReverseMap();
             CreateMap<Category, CategoryDto>();
             CreateMap<Category, CategoryListVm>();
             CreateMap<Category, CategoryEventListVm>();
+            
         }
     }
 }
